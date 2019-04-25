9b682b37d771263562bdb7a5b7fc600beaf128b7
Soojeong Choe <soojeong8@gmail.com>
2019-04-25 20:49:03 +0000

    starting routes

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,10 +1,30 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function (table) {
-    table.increments();
-    table.string('name');
-  });
-};
+  return Promise.all([
+  	//users
+  	knex.schema.createTable('users', function(table){
+  		table.increments();
+  		table.string('first_name', 50);
+  		table.string('last_name', 50);
+  		table.string('user_name', 20);
+  		table.string('email', 50);
+  		table.string('password', 50);
+  	}), 
+  	knew.schema.createTable('resources', function(table){
+  		table.increments();
+  		// table.bigInteger('created_by').unsigned().index().references('id').inTable('users').onDelete('cascade');
+  		table.string('url', 250);
+  		table.string('title', 50);
+  		table.string('description', 250);
+  		table.integer('user_id').references('users.id');
+  	})
+  ])
+}
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
+	return Promise.all([
+		knex.schema.dropTable('users'),
+		knex.schema.dropTable('resources')
+	])
 };
+
+
