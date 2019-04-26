ec38538884fb23b658a82e1673a2cd315e7d8ae8
Majid <mohammadmajid55@gmail.com>
2019-04-26 18:08:46 +0000

    found folder management bug for styles

--- routes/resources.js
+++ routes/resources.js
@@ -1,24 +1,24 @@
-"use strict";
+// "use strict";
 
-const express = require('express');
-const router  = express.Router();
-const bcrypt = require("bcrypt");
-const url = require('url');
+// const express = require('express');
+// const router  = express.Router();
+// const bcrypt = require("bcrypt");
+// const url = require('url');
 
 
-module.exports = (knex) => {
+// module.exports = (knex) => {
 
-	//get resource
-	router.get("/:id", (req, res) => {
-      knex
-        .select('resources.id','resources.url','resources.title','resources.description','users.user_name',
+// 	//get resource
+// 	router.get("/:id", (req, res) => {
+//       knex
+//         .select('resources.id','resources.url','resources.title','resources.description','users.user_name',
 
-	})
+// 	})
 
-	//create resource
+// 	//create resource
 
-	//update resource
+// 	//update resource
 
-	//delete resource
+// 	//delete resource
 
-}
+// }
